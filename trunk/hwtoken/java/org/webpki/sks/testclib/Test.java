9ea35a9b9ed1e07d31618d569c2b3b4a6328c863
andersphone <andersphone@40de300a-4973-11de-8001-7f2440edb24e>
2009-12-25 20:28:47 +0000

    Java SHA256 test added

--- trunk/hwtoken/java/org/webpki/sks/testclib/Test.java
+++ trunk/hwtoken/java/org/webpki/sks/testclib/Test.java
@@ -1,6 +1,7 @@
 package org.webpki.sks.testclib;
 
 import java.security.SecureRandom;
+import java.security.MessageDigest;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
@@ -72,32 +73,104 @@ public class Test
             random.nextBytes (msg_35);
             byte[] msg_300 = new byte[300];
             random.nextBytes (msg_300);
-
+        
             singleAES (raw_key, iv, msg_3, true);
             singleAES (raw_key, iv, msg_16, true);
             singleAES (raw_key, iv, msg_32, true);
             singleAES (raw_key, iv, msg_35, true);
             singleAES (raw_key, iv, msg_300, true);
-
-// Illegal            singleAES (raw_key, iv, msg_3, false);
+    
+    //Illegal            singleAES (raw_key, iv, msg_3, false);
             singleAES (raw_key, iv, msg_16, false);
             singleAES (raw_key, iv, msg_32, false);
-
+    
             singleAES (raw_key, null, msg_3, true);
             singleAES (raw_key, null, msg_16, true);
             singleAES (raw_key, null, msg_32, true);
             singleAES (raw_key, null, msg_35, true);
             singleAES (raw_key, null, msg_300, true);
-
-// Illegal            singleAES (raw_key, iv, msg_3, false);
+    
+    //Illegal            singleAES (raw_key, iv, msg_3, false);
             singleAES (raw_key, null, msg_16, false);
             singleAES (raw_key, null, msg_32, false);
           }
         System.out.println ("AES Testing Passed!");
       }
 
+    private String toHexCond (byte[] value) throws Exception
+      {
+        if (value == null) return "'null'";
+        return DebugFormatter.getHexDebugData (value);
+      }
+    
+    private void singleSHA256 (byte[] a, byte[] b, byte[] c, byte[] d) throws Exception
+      {
+        SHA256Provider sha256 = new SHA256Provider ();
+        MessageDigest md = MessageDigest.getInstance ("SHA-256");
+        sha256.update (a);
+        md.update (a);
+        if (b != null)
+          {
+            sha256.update (b);
+            md.update (b);
+          }
+        if (c != null)
+          {
+            sha256.update (c);
+            md.update (c);
+          }
+        if (d != null)
+          {
+            sha256.update (d);
+            md.update (d);
+          }
+        byte[] res = sha256.doFinal ();
+        byte[] jres = md.digest ();
+        
+        if (!ArrayUtil.compare (res, jres))
+          {
+            System.out.println ("Failed SHA256");
+            System.out.println ("a=\n" + toHexCond (a) +
+                              "\nb=\n" + toHexCond (b) +
+                              "\nc=\n" + toHexCond (c) +
+                              "\nd=\n" + toHexCond (d));
+            System.exit (-3);
+          }
+        sha256.dispose ();
+      }
+
+    private void testSHA256 () throws Exception
+      {
+        System.out.println ("SHA256 Testing Begins");
+        for (int i = 0; i < passes; i++)
+          {
+            int key_len = (i & 1) * 32 + 3;
+            SecureRandom random = new SecureRandom();
+            byte[] raw_key = new byte[key_len];
+            random.nextBytes (raw_key);
+            byte[] msg_3 = new byte[3];
+            random.nextBytes (msg_3);
+            byte[] msg_16 = new byte[16];
+            random.nextBytes (msg_16);
+            byte[] msg_32 = new byte[32];
+            random.nextBytes (msg_32);
+            byte[] msg_35 = new byte[35];
+            random.nextBytes (msg_35);
+            byte[] msg_300 = new byte[300];
+            random.nextBytes (msg_300);
+
+            singleSHA256 (raw_key, null, null, null);
+            singleSHA256 (msg_3,   null, null, null);
+            singleSHA256 (msg_3,   raw_key, null, null);
+            singleSHA256 (msg_3,   raw_key, msg_300, null);
+          }
+        System.out.println ("SHA256 Testing Passed!");
+      }
+
     public static void main (String[] argc) throws Exception
       {
-        new Test (1000).testAES ();
+        Test t = new Test (1000);
+        t.testAES ();
+        t.testSHA256 ();
       }
   }
