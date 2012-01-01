45aac176ec3b2d1986a88fae9c98a6981cbc1fe8
andersphone <andersphone@40de300a-4973-11de-8001-7f2440edb24e>
2012-01-01 07:55:53 +0000

    2012 upgrade - Happy New Year!

--- trunk/library/src/org/webpki/keygen2/PlatformNegotiationResponseEncoder.java
+++ trunk/library/src/org/webpki/keygen2/PlatformNegotiationResponseEncoder.java
@@ -1,6 +1,6 @@
 package org.webpki.keygen2;
 /*
- *  Copyright 2006-2011 WebPKI.org (http://webpki.org).
+ *  Copyright 2006-2012 WebPKI.org (http://webpki.org).
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
