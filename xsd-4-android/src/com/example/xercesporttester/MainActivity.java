051d08307478c98ae6e2175dd9dec691fe3b056e
andersphone <andersphone@40de300a-4973-11de-8001-7f2440edb24e>
2012-10-15 09:46:52 +0000

    0.0

---
+++ xsd-4-android/src/com/example/xercesporttester/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.xercesporttester;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    
+}
