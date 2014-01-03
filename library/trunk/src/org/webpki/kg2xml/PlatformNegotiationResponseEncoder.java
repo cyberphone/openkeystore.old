fbed30a71d2308cf0396b439beab421577b22bf0
andersphone <andersphone@40de300a-4973-11de-8001-7f2440edb24e>
2014-01-03 03:53:23 +0000

    2014!

--- library/trunk/src/org/webpki/kg2xml/PlatformNegotiationResponseEncoder.java
+++ library/trunk/src/org/webpki/kg2xml/PlatformNegotiationResponseEncoder.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2006-2013 WebPKI.org (http://webpki.org).
+ *  Copyright 2006-2014 WebPKI.org (http://webpki.org).
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
  */
 package org.webpki.kg2xml;
 /*
- *  Copyright 2006-2013 WebPKI.org (http://webpki.org).
+ *  Copyright 2006-2014 WebPKI.org (http://webpki.org).
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
