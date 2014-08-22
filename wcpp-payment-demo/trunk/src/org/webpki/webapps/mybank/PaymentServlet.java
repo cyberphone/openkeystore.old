fd4addf9a3b2112483118cf0fe82f14836c0ea65
andersphone <andersphone@40de300a-4973-11de-8001-7f2440edb24e>
2014-08-22 17:15:46 +0000

    r5

---
+++ wcpp-payment-demo/trunk/src/org/webpki/webapps/mybank/PaymentServlet.java
@@ -0,0 +1,19 @@
+package org.webpki.webapps.mybank;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PaymentServlet extends HttpServlet
+  {
+    private static final long serialVersionUID = 1L;
+    
+    public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+      {
+        HTML.paymentPage (response, request);
+      }
+  }
