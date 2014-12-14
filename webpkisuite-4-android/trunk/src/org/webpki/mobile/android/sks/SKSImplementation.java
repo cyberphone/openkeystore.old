9967ee96bc49d318d64b5c451322aac10da3e68e
andersphone <andersphone@40de300a-4973-11de-8001-7f2440edb24e>
2014-12-14 18:57:02 +0000

    changed rsa.es uri

--- webpkisuite-4-android/trunk/src/org/webpki/mobile/android/sks/SKSImplementation.java
+++ webpkisuite-4-android/trunk/src/org/webpki/mobile/android/sks/SKSImplementation.java
@@ -984,7 +984,7 @@ public class SKSImplementation implements SKSError, SecureKeyStore, Serializable
         //////////////////////////////////////////////////////////////////////////////////////
         //  Asymmetric Key Decryption
         //////////////////////////////////////////////////////////////////////////////////////
-        addAlgorithm ("http://xmlns.webpki.org/sks/algorithm#rsa.pkcs1_5",
+        addAlgorithm ("http://xmlns.webpki.org/sks/algorithm#rsa.es.pkcs1_5",
                       "RSA/ECB/PKCS1Padding",
                       ALG_ASYM_ENC | ALG_RSA_KEY);
 
