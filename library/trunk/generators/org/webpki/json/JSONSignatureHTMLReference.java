ef4d790fbd826603283d08e0a3b89a383d9d96bb
andersphone <andersphone@40de300a-4973-11de-8001-7f2440edb24e>
2014-12-18 12:56:02 +0000

    jcs wording

--- library/trunk/generators/org/webpki/json/JSONSignatureHTMLReference.java
+++ library/trunk/generators/org/webpki/json/JSONSignatureHTMLReference.java
@@ -58,7 +58,7 @@ public class JSONSignatureHTMLReference extends JSONBaseHTML.Types
             "approaches, but for information-rich messaging, " +
             "cleartext data at least have an advantage for documentation and debugging. " +
             "To cope with the primary disadvantage (the potential dependency on " +
-            "canonicalization), this part has been extremely simplified compared to XML&nbsp;DSig. "+
+            "canonicalization), this part has been extremely simplified. "+
             "In fact, JCS doesn't actually rely on canonicalization since it doesn't serve any purpose " +
             "in this particular context.");
 
