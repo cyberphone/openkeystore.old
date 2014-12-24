724f719611c5bd961733dd76fcdc9f87366ab385
andersphone <andersphone@40de300a-4973-11de-8001-7f2440edb24e>
2014-12-24 06:53:49 +0000

    Christmas edition :-)

--- python/trunk/src/org/webpki/json/JCSValidator.py
+++ python/trunk/src/org/webpki/json/JCSValidator.py
@@ -16,28 +16,31 @@ class new:
 
   def __init__(self,jsonObject):
     if not isinstance(jsonObject, collections.OrderedDict):
-        raise TypeError('You must use "collections.OrderedDict"')
+      raise TypeError('JCS requires JSON to be parsed into a "collections.OrderedDict"')
     signatureObject = jsonObject['signature']
     clonedSignatureObject = collections.OrderedDict(signatureObject)
     signatureAlgorithm = signatureObject['algorithm']
     if signatureAlgorithm != 'RS256':
-        raise TypeError('Only the "RS256" algorithm is currently supported')
+      raise TypeError('Only the "RS256" algorithm is currently supported')
     publicKey = signatureObject['publicKey']
     if publicKey['type'] != 'RSA':
-       raise TypeError('Only "RSA" keys are currently supported')
+      raise TypeError('Only "RSA" keys are currently supported')
     self.rsaPublicKey = RSA.construct([Utils.getCryptoBigNum(publicKey['n']),Utils.getCryptoBigNum(publicKey['e'])])
     rsaVerifier = PKCS1_v1_5.new(self.rsaPublicKey)
     signatureValue = Utils.base64UrlDecode(signatureObject['value'])
     signatureObject.pop('value')
-    signatureData = json.dumps(jsonObject,separators=(',',':'),ensure_ascii=False).encode("utf-8")
+    self.normalizedData = json.dumps(jsonObject,separators=(',',':'),ensure_ascii=False)
     jsonObject['signature'] = clonedSignatureObject
-    self.valid = rsaVerifier.verify(SHA256.new(signatureData),signatureValue)
+    self.valid = rsaVerifier.verify(SHA256.new(self.normalizedData.encode("utf-8")),signatureValue)
 
   def isValid(self):
     return self.valid
 
   def getPublicKey(self):
     return self.rsaPublicKey
+
+  def getNormalizedData(self):
+    return self.normalizedData
 
 # TODO: "extensions", "version", "keyId" and checks for extranous properties
 
--- python/trunk/test/validate.py
+++ python/trunk/test/validate.py
@@ -14,8 +14,20 @@ jsonString = codecs.open(sys.argv[1], "r", "utf-8").read()
 
 # print jsonString
 
+def checkAllSignatures(jsonObject):
+    for w in jsonObject:
+       if w == 'signature':
+          validator = JCSValidator.new(jsonObject)
+          print 'Valid=' + str(validator.isValid()) + ' Normalized data=\n' + validator.getNormalizedData()
+       if isinstance(jsonObject[w],collections.OrderedDict):
+         checkAllSignatures(jsonObject[w])
+
 jsonObject = json.loads(jsonString, object_pairs_hook=collections.OrderedDict)
 result = JCSValidator.new(jsonObject)
 print 'Valid=' + str(result.isValid())
 if result.isValid():
   print 'Key=' + str(result.getPublicKey().exportKey(format='PEM', passphrase=None, pkcs=1))
+#  print result.getNormalizedData () # Fails on Windows unless you have the "Lucida Console" font
+
+# checkAllSignatures(jsonObject)
+
