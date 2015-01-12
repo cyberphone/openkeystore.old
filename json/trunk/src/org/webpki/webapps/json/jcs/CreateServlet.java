28f7eac4cfea4b12b75f9bc0a36d2bda657e0f14
andersphone <andersphone@40de300a-4973-11de-8001-7f2440edb24e>
2015-01-12 20:47:25 +0000

    minor redundance removed

--- json/trunk/src/org/webpki/webapps/json/jcs/CreateServlet.java
+++ json/trunk/src/org/webpki/webapps/json/jcs/CreateServlet.java
@@ -20,14 +20,15 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.webpki.json.JSONObjectWriter;
 import org.webpki.json.JSONParser;
+
 import org.webpki.util.Base64URL;
-import org.webpki.webutil.ServletUtil;
 
 public class CreateServlet extends HttpServlet
   {
