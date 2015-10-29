81379ec4502b6beaa834f6e51ed3104f2b36047c
Anders Rundgren <anders.rundgren.net@gmail.com>
2015-10-29 18:22:00 +0100

    es6 looking better

--- es6-numbers/src/com/example/es6numbers/Test.java
+++ es6-numbers/src/com/example/es6numbers/Test.java
@@ -5,74 +5,125 @@ import java.io.FileOutputStream;
 import javax.script.*;
 
 public class Test {
+
+    public static String es6DoubleSerialization(double value) {
+
+        // 1. Take care of the sign
+        String hyphen = "";
+        if (value < 0) {
+            value = -value;
+            hyphen = "-";
+        }
+
+        // We may need to start-over due to rounding of the dropped 16:th digit
+        boolean round = true;
+        while (true) {
+
+            // 2. Serialize using Java default
+            StringBuffer num = new StringBuffer(Double.toString(value));
+
+            // 4. Collect and remove the optional exponent
+            int exp = 0;
+            int i = num.indexOf("E");
+            if (i > 0) {
+                int j = i;
+                if (num.indexOf("-") > 0) {
+                    j++;
+                }
+                exp = Integer.valueOf(num.substring(j + 1));
+                if (j != i) {
+                    exp = -exp;
+                }
+                num.delete(i, num.length());
+            }
+
+            // 5. There must be a decimal point.  Remove it from the string and record the position
+            int dp = num.indexOf(".");
+            num.deleteCharAt(dp);
+
+            // 6. Normalize decimal point to position 0
+            exp += dp;
+            dp = 0;
+
+            // 7. Normalize number so that MSD != 0
+            int lastNonZero = 0;
+            i = 0;
+            while (i < num.length()) {
+                if (num.charAt(0) == '0') {
+                    num.deleteCharAt(0);
+                    exp--;
+                } else {
+                    if (num.charAt(i) != '0') {
+                        lastNonZero = i;
+                    }
+                    i++;
+                }
+            }
+
+            // 8. Check if we got nothing (0)
+            if (num.length() == 0) {
+                return "0";
+            }
+
+            // 9. Check digit 16 for rounding
+            if (round && lastNonZero >= 15 && num.charAt(15) >= '5') {
+                value += Math.pow(10, exp - 15) / 2;
+                round = false;
+                continue;
+            }
+
+            // 10. Remove digits beyond 15
+            if (lastNonZero >= 15) {
+                num.delete(15, num.length());
+            }
+
+            // 11. Remove trailing zeroes
+            while (num.charAt(num.length() - 1) == '0') {
+                num.deleteCharAt(num.length() - 1);
+            }
+
+            // 12. Put decimal point and add missing zeroes if needed
+            int len = num.length();
+            if (exp >= len && exp <= 21) {
+                // It is an integer, drop decimal point
+                exp -= len;
+                while (exp > 0) {
+                    num.append('0');
+                    exp--;
+                }
+                dp = -1;
+            }
+            else if (exp <= 0 && exp > -6 && len - exp < 21) {
+                while (exp < 0) {
+                    num.insert(0, '0');
+                    exp++;
+                }
+            } else if (exp < 0) {
+                dp = len == 1 ? -1 : 1;
+                exp--;
+            } else if (exp < len) {
+                dp = exp;
+                exp = 0;
+            } else {
+                dp = 1;
+                exp--;
+            }
+
+            if (exp != 0) {
+                num.append('e').append(exp > 0 ? "+" : "").append(exp);
+            }
+            if (dp == 0) {
+                num.insert(0, "0.");
+            } else if (dp > 0) {
+                num.insert(dp, '.');
+            }
+            return num.insert(0, hyphen).toString();
+        }
+    }
+
     static ScriptEngine engine;
     
     static int d15Errors;
-
-    private static String dtoa(double d) throws Exception {
-        String hyphen = "";
-        if (d < 0) {
-            d = -d;
-            hyphen = "-";
-        }
-        d = d + (d / 1e15) / 2;
-        String num = Double.toString(d);
-        int exp = 0;
-        int i = num.indexOf('E');
-        if (i > 0) {
-            int j = i;
-            if (num.indexOf('-') > 0) {
-                j++;
-            }
-            exp = Integer.valueOf(num.substring(j + 1));
-            if (j != i) {
-                exp = -exp;
-            }
-            num = num.substring(0, i);
-        }
-        int dp = num.indexOf('.');
-        num = num.substring(0, dp) + num.substring(dp + 1);
-        int len = num.length();
-        int first = -1;
-        int last = -1;
-        for (int q = 0; q < num.length(); q++) {
-            if (num.charAt(q) != '0') {
-                if (first < 0) {
-                    first = q;
-                }
-                last = q;
-            }
-        }
-        if (last - first >= 15) {
-            num = num.substring(0, first + 15);
-            for (int q = 0; q <= last - first - 15; q++) {
-                num += "@";
-            }
-        }
-        if (exp >= (len - dp) && exp < 21) {
-            System.out.println("h=" + (exp + dp));
-            exp -= (len - dp - 1);
-            while (--exp > 0) {
-                num += "#";
-            }
-            dp = 0;
-        } else if (exp == 0) {
-            // if (num.substring(dp)
-        }
-        StringBuffer s = new StringBuffer();
-        if (len - dp == 1 && num.charAt(dp) == '0') {
-            num = num.substring(0, dp);
-            dp = 0;
-        }
-        s.append(num);
-        if (dp > 0) {
-            s.insert(dp, '.');
-        }
-        if (exp != 0) {
-            s.append('e').append(exp > 0 ? "+" : "").append(exp);
-        }
-        return s.insert(0, hyphen).toString();
-    }
 
     static FileOutputStream fos;
 
@@ -108,7 +159,7 @@ public class Test {
         engine.put("fl", d);
         engine.eval("res=parseFloat(fl.toPrecision(15)).toString()");
         String js = engine.get("res").toString();
-        String d15 = dtoa(d);
+        String d15 = es6DoubleSerialization(d);
         if (!compare(js, d15)) {
             d15 = bad(d15);
             d15Errors++;
@@ -149,8 +200,24 @@ public class Test {
         }
         test(10);
         test(0);
-        test(0.0005);
-        test(0.0000000005);
+        for (int i = 0; i < 10; i++) {
+            test(5.0 / Math.pow(10, i));
+        }
+        test(0.00000506);
+        test(0.000005006);
+        test(0.0000050006);
+        test(0.00000500006);
+        test(0.000005000006);
+        test(0.0000050000006);
+        test(0.00000500000006);
+        test(0.000005000000006);
+        test(0.0000050000000006);
+        test(0.00000500000000006);
+        test(0.000005000000000006);
+        test(0.0000050000000000006);
+        test(0.00000500000000000006);
+        test(0.000005000000000000006);
+        test(0.0000050000000000000006);
         test(0.999999999999999999999999999);
         test(-0.999999999999999999999999999);
         test(-0.9999999999999993);
