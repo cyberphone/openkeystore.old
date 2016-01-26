49c9b30303f973e606d19f89816f78de1b4ed2f7
Anders Rundgren <anders.rundgren.net@gmail.com>
2016-01-26 06:45:45 +0100

    Python 3.5 support

---
+++ python/3.5/test/acme-csr-demo.py
@@ -0,0 +1,20 @@
+# This is a short program showing a possible CSR using JCS for the
+# ACME (Automatic Certificate Management Environment) system
+
+theKey = (
+'{'
+'  "kty":"EC",'
+'  "crv":"P-256",'
+'  "x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",'
+'  "y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",'
+'  "d":"870MB6gfuTJ4HtUnUvYMyJpr5eUZNP4Bk43bVdj3eAE"'
+'}')
+
+from org.webpki.json import SignatureKey
+from org.webpki.json.Writer import JSONObjectWriter
+
+jsonObject = JSONObjectWriter().setString("@context","https://letsencrypt.org/acme/v1").setString("@qualifier","CertificateRequest")
+jsonObject.setString("domain","example.com")
+jsonObject.setBinary("secret",b'\x56\x23\x23\x00\x10');
+jsonObject.setSignature(SignatureKey.new(theKey))
+print (jsonObject.serialize())
