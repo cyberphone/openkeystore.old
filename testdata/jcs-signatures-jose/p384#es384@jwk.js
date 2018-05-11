1d36820cb33399829e17067c21388b1848e7912c
Anders Rundgren <anders.rundgren.net@gmail.com>
2018-05-11 17:34:04 +0200

    Algorithm update

---
+++ testdata/jcs-signatures-jose/p384#es384@jwk.js
@@ -0,0 +1,14 @@
+{
+  statement: "Hello Signed World!",
+  otherProperties: [2000,true],
+  __cleartext_signature: {
+    alg: "ES384",
+    jwk: {
+      kty: "EC",
+      crv: "P-384",
+      x: "GLfdsvEwphRzS_twup7UFPVOk7_CKgHZ7dt_fJ2QHPBdJa1c5pfJcRIWTfT0lpg9",
+      y: "ovA5_QXmFbj9U4pjZ1AX_ZdVyIRZUBWW9cuZda_tupKfWQfmcQHzDmHGHbxl9Xxl"
+    },
+    signature: "IJLXDFQffCBaXUs_jHoJxGTTR6kVqDYMrbNeR-_29WJIdJX12PB2u1Xge3sPyOWslYxDyFJEltB807Ocyr2s6Sx0JFk2BX8PHoS4Xt_YKIatcjideDjtVEoaoLzvs-Kh"
+  }
+}
---
+++ testdata/jcs-signatures/p384#es384@jwk.js
@@ -0,0 +1,14 @@
+{
+  statement: "Hello Signed World!",
+  otherProperties: [2000,true],
+  signature: {
+    alg: "ES384",
+    jwk: {
+      kty: "EC",
+      crv: "P-384",
+      x: "GLfdsvEwphRzS_twup7UFPVOk7_CKgHZ7dt_fJ2QHPBdJa1c5pfJcRIWTfT0lpg9",
+      y: "ovA5_QXmFbj9U4pjZ1AX_ZdVyIRZUBWW9cuZda_tupKfWQfmcQHzDmHGHbxl9Xxl"
+    },
+    val: "IXWOmVi9trI-5OQYrDyYO4v6kTMtPSxYbjnIhRdtJCvT2ODajovHq_eE1kbLhl7kvLQWJgfLM-m_bC53_FVJjLd5KxCnyT1xQ3nHSKRLmXxgdILrGEDlLNFOaz_EMfM_"
+  }
+}
